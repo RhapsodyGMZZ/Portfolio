59a4595b339420a22447b6315fae2f36eaf53f4b
nlegay <legaynicolas76@gmail.com>
2024-04-12 08:48:56 +0000

    Added Loading template to avoid lagging-impression loading

--- services/frontend/app/layout.tsx
+++ services/frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AnimatePresence, motion } from "framer-motion";
 import { Suspense } from "react";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-slate-50`}>
-        {children}
+        <Suspense fallback={<Loading />}>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
