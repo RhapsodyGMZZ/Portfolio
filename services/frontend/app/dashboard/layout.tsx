79bc7c1cf7343e084138da777346de8ecaa0c612
n.legay <nicolas.legay@hafa.fr>
2025-01-28 12:21:26 +0100

    scroll in main menu fix

--- services/frontend/app/dashboard/layout.tsx
+++ services/frontend/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Header />
             <main className="flex flex-col md:flex-row lg:overflow-y-hidden max-h-screen lg:max-h-[86vh]">
                 <SideNav />
-                <div className="flex-grow p-6 overflow-y-auto lg:overflow-y-hidden md:p-12">{children}</div>
+                <div className="flex-grow p-6 overflow-y-auto md:p-12">{children}</div>
             </main>
         </>
     )
